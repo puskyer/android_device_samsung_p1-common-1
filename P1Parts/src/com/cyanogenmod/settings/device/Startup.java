3bb00917e886dd34173fecc1bdddb9dac386277c
storm31 <sergej.michel@t-online.de>
2014-03-19 22:02:47 +0100

    - Screenlights and Buttonlights restore

--- P1Parts/src/com/cyanogenmod/settings/device/Startup.java
+++ P1Parts/src/com/cyanogenmod/settings/device/Startup.java
@@ -24,6 +24,7 @@ public class Startup extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
+        ToggleCapacitiveKeys.restore(context);
         TouchKeyBacklightTimeout.restore(context);
         FastCharge.restore(context);
         TouchscreenClock.restore(context);
