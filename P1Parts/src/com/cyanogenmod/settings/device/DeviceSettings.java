4f9316ad0452f051785016174191a6944eaba34d
storm31 <sergej.michel@t-online.de>
2014-04-03 00:19:27 +0200

    - try to merge Network traffic

--- P1Parts/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ P1Parts/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -82,6 +82,8 @@ public class DeviceSettings extends Activity {
                 TVFragmentActivity.class, null);
         mTabsAdapter.addTab(bar.newTab().setText(R.string.category_usb_title),
                 FastChargeFragmentActivity.class, null);
+        mTabsAdapter.addTab(bar.newTab().setText(R.string.category_network_traffic),
+                NetworkTrafficFragmentActivity.class, null);
         if (savedInstanceState != null) {
             bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
         }
