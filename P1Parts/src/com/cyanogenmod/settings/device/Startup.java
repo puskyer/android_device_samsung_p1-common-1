8e53cff12afda24100a83b9ebb1855276bed0e5a
storm31 <sergej.michel@t-online.de>
2014-09-16 07:47:45 +0200

    Update Startup.java

--- P1Parts/src/com/cyanogenmod/settings/device/Startup.java
+++ P1Parts/src/com/cyanogenmod/settings/device/Startup.java
@@ -26,7 +26,7 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent bootintent) {
         ToggleCapacitiveKeys.restore(context);
         TouchKeyBacklightTimeout.restore(context);
-		KeyLedBrightness.restore(context);
+	KeyLedBrightness.restore(context);
         FastCharge.restore(context);
         ZramDisable.restore(context);
         ZramNumDevice.restore(context);
