7bf503c1f9d008760809bd932fb1a3a92b0851e9
storm31 <sergej.michel@t-online.de>
2014-12-06 21:58:22 +0100

    - twrp

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,13 +83,13 @@ BOARD_NAND_PAGE_SIZE := 4096
 BOARD_NAND_SPARE_SIZE := 128
 BOARD_KERNEL_BASE := 0x32000000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_CMDLINE := console=ttyFIQ0,115200 init=/init no_console_suspend
-
+BOARD_KERNEL_CMDLINE := console=ttyFIQ0,115200 androidboot.selinux=permissive init=/init no_console_suspend
 BOARD_BOOTIMAGE_PARTITION_SIZE := 7864320
 BOARD_FLASH_BLOCK_SIZE := 4096
 
 # Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/p1-common
